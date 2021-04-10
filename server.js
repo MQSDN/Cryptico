04805af03c160db39ee79aaa3b7c0a59068d54d2
qusaiqishta <qeshtaqusai0@gmail.com>
2021-04-10 20:39:19 +0300

    login feature done

--- server.js
+++ server.js
@@ -1,40 +1,162 @@
-'use strict';
-
-// All Dependencies Set-up
-const express = require('express');
-const superAgent = require('superagent');
-const cors = require('cors');
-const pg = require('pg');
-const methodOverride = require('method-override');
+const express = require("express");
+const { pool } = require("./dbConfig");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+require("dotenv").config();
 const app = express();
-require('dotenv').config();
-
-
-const PORT = process.env.PORT;
-
-
-// MiddleWares
-app.use(express.urlencoded({ extended: true }));
-// app.use(express.static());
-app.use(methodOverride('_method'));
-app.set('view engine', 'ejs');
 
 
 
 
+const PORT = process.env.PORT || 3000;
 
+const initializePassport = require("./passportConfig");
 
+initializePassport(passport);
 
+// Middleware
 
+// Parses details from a form
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
+app.use(
+  session({
+    // Key we want to keep secret which will encrypt all of our information
+    secret: process.env.SESSION_SECRET,
+    // Should we resave our session variables if nothing has changes which we dont
+    resave: false,
+    // Save empty value if there is no vaue which we do not want to do
+    saveUninitialized: false
+  })
+);
+// Funtion inside passport which initializes passport
+app.use(passport.initialize());
+// Store our variables to be persisted across the whole session. Works with app.use(Session) above
+app.use(passport.session());
+app.use(flash());
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
+app.get("/users/register", checkAuthenticated, (req, res) => {
+  res.render("register.ejs");
+});
 
+app.get("/users/login", checkAuthenticated, (req, res) => {
+  // flash sets a messages variable. passport sets the error message
+  res.render("login.ejs");
+});
 
+app.get("/users/profile", checkNotAuthenticated, (req, res) => {
+  console.log(req.isAuthenticated());
+  res.render("profile", { user: req.user.name });
+});
 
+app.get("/users/logout", (req, res) => {
+  req.logout();
+  res.render("index", { message: "You have logged out successfully" });
+});
 
+app.post("/users/register", async (req, res) => {
+  let { name, email, password, password2 } = req.body;
 
+  let errors = [];
 
+  console.log({
+    name,
+    email,
+    password,
+    password2
+  });
 
-//  PORT
-app.listen(PORT, () => console.log('Listening to PORT ' + PORT));
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: "Please enter all fields" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ message: "Password must be a least 6 characters long" });
+  }
+
+  if (password !== password2) {
+    errors.push({ message: "Passwords do not match" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", { errors, name, email, password, password2 });
+  } else {
+    hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+    // Validation passed
+    pool.query(
+      `SELECT * FROM users
+        WHERE email = $1`,
+      [email],
+      (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+
+        if (results.rows.length > 0) {
+          return res.render("register", {
+            message: "Email already registered"
+          });
+        } else {
+          pool.query(
+            `INSERT INTO users (name, email, password)
+                VALUES ($1, $2, $3)
+                RETURNING id, password`,
+            [name, email, hashedPassword],
+            (err, results) => {
+              if (err) {
+                throw err;
+              }
+              req.flash("success_msg", "You are now registered. Please log in");
+              res.redirect("/users/login");
+            }
+          );
+        }
+      }
+    );
+  }
+});
+
+app.post(
+  "/users/login",
+  passport.authenticate("local", {
+    successRedirect: "/users/profile",
+    failureRedirect: "/users/login",
+    failureFlash: true
+  })
+);
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
+  }
+  next();
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/users/login");
+}
+
+function myFunction() {
+    var x = document.getElementById("Password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+  myFunction();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
