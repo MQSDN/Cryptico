a2210556b23ca28c2ed587d8ed831fe0d317bba4
qusaiqishta <qeshtaqusai0@gmail.com>
2021-04-11 11:49:48 +0300

    login done

--- schema.sql
+++ schema.sql
@@ -1,13 +1,15 @@
 Drop table if exists users;
-Create table if not exists users(id Primary Key serial not null,
+Create table users(id serial primary key not null,
 name VARCHAR(200) not null,
 email VARCHAR(200) not null,
-password VARCHAR(200) not null);
+pass VARCHAR(200) not null,
+date date not null
+);
 
 
 Drop table if exists userProfile;
-Create table if not exists userProfile (userResult Numeric);
+Create table   userProfile (userResult Numeric);
 
 
-Drop table if not exists quiz;
-Create table if not exists (question text, optionA text, optionB text, optionC text, optionD text, correctAnswer text);
+Drop table if exists quiz;
+Create table quiz  (question text, optionA text, optionB text, optionC text, optionD text, correctAnswer text);
